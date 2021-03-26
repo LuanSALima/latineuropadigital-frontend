597f20882e770ddea6389fcfaea1d8cdefc5af7f
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-03-26 15:38:42 -0300

    Horizon Scroll View is Automatic in NoticeList

--- src/pages/NoticesList/index.js
+++ src/pages/NoticesList/index.js
@@ -1,79 +1,96 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import NoticesCard from '../../components/NoticesCard';
-import { Page, ScreenView } from '../../styles/default';
+import { AppButton, Page, ScreenView } from '../../styles/default';
 import imgTest from '../../assets/icon.svg';
-import Footer from '../../components/Footer';
+
 import api from '../../services/api';
 import HorizonScrollView from '../../components/HorizonScrollView';
 
 function NoticesList() {
-  //Card content
-  /*
-  const [myCard,setMyCard] = useState([{
-    title:"LatiN Europa Digital Vem com Tudo!".substr(0,18),
-    description:"Latin Europa Digital vem fazendo Juz ao Nome! com seu Criador Oficcial, Alexandre, Conectou Continentes!".substr(0,75),
-    image:imgTest,
-    icon:imgTest,
-  },
-  {
-    title:"LatiN Europa Digital Vem com Tudo!".substr(0,18),
-    description:"Latin Europa Digital vem fazendo Juz ao Nome! com seu Criador Oficcial, Alexandre, Conectou Continentes!".substr(0,75),
-    image:imgTest,
-    icon:imgTest,
-  }]);
-  */
 
   const [posts, setPosts] = useState([]);
+  const [tags, setTags] = useState([]);
   const [errors, setErrors] = useState({});
+  const [loading,setLoading] = useState(false);
+ 
+ 
+  const listTags = async () =>{
+    try {
+      setLoading(true);
+      const response = await api.get("/tag/list");
+      setTags(response.data.tags)
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  }
+   const listPosts = async () => {
+    try {
+      setLoading(true);
 
-  useEffect(() => {
+      const response = await api.get("/post/list");
 
-    async function listPosts() {
-      try {
-        const response = await api.get("/post/list");
-
-        if(response.data.success) {
-          if(response.data.posts) {
-            var postsDb = [];
-            for(let index in response.data.posts) {
-              const post = response.data.posts[index];
-              postsDb.push({id: post._id, title: post.title, description: post.description, image: `${process.env.REACT_APP_API_URL}`+post.imagePath, icon: imgTest});
-            }
-            setPosts(postsDb);
+      if(response.data.success) {
+        if(response.data.posts) {
+          let postsDb = [];
+          for(let index in response.data.posts) {
+            const post = response.data.posts[index];
+            postsDb.push({ tag : post.tags,id: post._id, title: post.title, description: post.description, image: `${process.env.REACT_APP_API_URL}`+post.imagePath, icon: imgTest});
           }
+          setPosts(postsDb);
         }
-      } catch (error) {
-        if(error.response) {
-          if(error.response.data) {
-            if(error.response.data.message) {
-              setErrors({message: error.response.data.message});
-            }
+      }
+      setLoading(false);
+
+    } catch (error) {
+      setLoading(false);
+
+      if(error.response) {
+        if(error.response.data) {
+          if(error.response.data.message) {
+            setErrors({message: error.response.data.message});
           }
         }
       }
     }
+  }
+
+  useEffect(() => {
     listPosts();
+  }, []);
+  useEffect(() => {
+    listTags();
   }, []);
 
   return (
       <Page>
         <Header/>
         <ScreenView width={"90%"}>
+          <AppButton type="button" onClick={()=>{
+            console.log(posts);
+            console.log(tags);
+          }}>list</AppButton>
+          {loading &&<h1>Loading..</h1>}
          {/* <FeatureContent>
         <b>Acesse Nossa PÁGINA!!!</b>
          </FeatureContent> */}
         <br></br>
         <h2 style={{color: 'red'}}>{errors.message}</h2>
+{tags.map((tags)=>(
+  <HorizonScrollView title={tags.title} subtitle="Espetáculos, entretenimento e mucho más">
+  {posts.map((content)=>(
+    content.tag.map((tagFiltered)=>{
+      if(tagFiltered === tags.title){
+        return  <NoticesCard id={content.id} icon={content.icon} image={content.image} title={content.title.length >= 18?content.title+"..":content.title}text={content.description.length >= 75 ? content.description+"..":content.description} />
+       }
+    })
+ 
+  ))}
+  </HorizonScrollView>
+  ))}
         
-        
-        <HorizonScrollView title="destaques" subtitle="Espetáculos, entretenimento e mucho más">
-        {posts.map((content)=>(
-          <NoticesCard id={content.id} icon={content.icon} image={content.image} title={content.title.length >= 18?content.title+"..":content.title}text={content.description.length >= 75 ? content.description+"..":content.description} />
-        ))}
-        </HorizonScrollView>
         </ScreenView>
-          <Footer/>
       </Page>
   );
 }
