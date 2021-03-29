b3f13b9e64756973f8972afea36e28f6d87663bd
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-03-29 12:10:53 -0300

    Create new design for oportunities and actualidad

--- src/pages/OpportunitieList/index.js
+++ src/pages/OpportunitieList/index.js
@@ -5,20 +5,17 @@ import { Page } from '../../styles/default';
 
 import api from '../../services/api';
 import Footer from '../../components/Footer';
+import { MyScreenView,OportunityCard } from './styles';
+
+
 function Job(props) {
   return (
-    <div style={{marginBottom: '15px'}}>
-      <span style={{fontSize: '18px'}}>Título: </span>
-      <span>{props.job.title}</span>
-      <br />
-      <span style={{fontSize: '18px'}}>Descrição: </span>
-      <span>{props.job.description}</span>
-      <br />
-      <span style={{fontSize: '18px'}}>Nome:</span>
-      <span>{props.job.professionalName}</span>
-      <span style={{fontSize: '18px', marginLeft: '10px'}}>Contato:</span>
-      <span>{props.job.professionalContact}</span>
-    </div>
+    <OportunityCard>
+      <label>{props.job.title}</label>
+      <span >Nome: <b>{props.job.professionalName}</b></span>
+      <span >Descrição: <b>{props.job.description}</b></span>
+      <span >Contato: <b>{props.job.professionalContact}</b></span>
+    </OportunityCard>
   );
 }
 
@@ -27,40 +24,45 @@ function OpportunitieList(props) {
   const [jobs, setJobs] = useState([]);
   const [errors, setErrors] = useState({});
 
-  useEffect(() => {
 
-    async function listJobs() {
-      try {
-        const response = await api.get("/job/list");
 
-        if(response.data.success) {
-          if(response.data.jobs) {
-            setJobs(response.data.jobs);
-          }
+  async function listJobs() {
+    try {
+      const response = await api.get("/job/list");
+
+      if(response.data.success) {
+        if(response.data.jobs) {
+          setJobs(response.data.jobs);
         }
-      } catch (error) {
-        setErrors({message: "Não foi possível carregar as Oportunidades"});
-        if(error.response) {
-          if(error.response.data) {
-            if(error.response.data.message) {
-              setErrors({message: error.response.data.message});
-            }
+      }
+    } catch (error) {
+      setErrors({message: "Não foi possível carregar as Oportunidades"});
+      if(error.response) {
+        if(error.response.data) {
+          if(error.response.data.message) {
+            setErrors({message: error.response.data.message});
           }
         }
       }
     }
-    listJobs();
+  }
 
+  useEffect(() => {
+    listJobs();
   }, []);
 
   return (
   <Page>
     <Header/>
-    <h1>Oportunidades</h1>
+    <MyScreenView>
+
+    <h1>OPORTUNIDADES</h1>
     <h2 style={{color: 'red'}}>{errors.message}</h2>
      {jobs.map((currentjob)=>(
         <Job job={currentjob} />
       ))}
+
+    </MyScreenView>
       <Footer/>
   </Page>);
 }
