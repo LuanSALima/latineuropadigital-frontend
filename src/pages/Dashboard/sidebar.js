ff206505ae164e49d3a5399691cecf789d48f0f4
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-03-29 14:46:32 -0300

    Finish sidebar style

--- src/pages/Dashboard/sidebar.js
+++ src/pages/Dashboard/sidebar.js
@@ -1,32 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import { SidebarStyled } from "./styles";
 
 function Sidebar(props) {
-  return  (
-      <Container>
- <div className="container-fluid mt-5">
-		<div className="row mt-3">
-			<div className="sidebar-sticky p-4">
-				<h3 className="sidebar-heading justify-content-center align-items-center text-center">Menu</h3>
-				<ul className="nav flex-column justify-content-center align-items-center">
-					<li className="nav-item p-1">
-						<button onClick={props.noticia}>Notícias</button>
-					</li>
-					<li className="nav-item p-1">
-						<button onClick={props.usuario}>Usuários</button>
-					</li>
-					<li className="nav-item p-1">
-						<button onClick={props.oportunidade}>Oportunidades</button>
-					</li>
-					<li className="nav-item p-1">
-						<button onClick={props.tag}>Tags</button>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-      </Container>
+  return (
+    <SidebarStyled>
+      <div onClick={props.noticia}>
+        <span>Notícias</span>
+      </div>
+      <div onClick={props.usuario}>
+        <span>Usuários</span>
+      </div>
+      <div onClick={props.oportunidade}>
+        <span>Oportunidades</span>
+      </div>
+      <div onClick={props.tag}>
+        <span>Tags</span>
+      </div>
+    </SidebarStyled>
   );
 }
 
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -3,3 +3,44 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+
+
+
+//for sidebar
+
+
+export const SidebarStyled = styled.div`
+min-width:13vw;
+width:auto;
+background-color:var(--color-freela-white);
+border-radius:0.3rem;
+display:flex;
+flex-direction:column;
+position:absolute;
+padding:0 !important;
+margin-top:0.5rem;
+left:0;
+height:80%;
+>div{
+    display:flex;
+    height:20%;
+    width:100%;
+    padding:0.3rem !important;
+    cursor:pointer;
+    justify-content:center;
+    text-align:center;
+    align-items:center;
+    transition: background-color 0.3s ease;
+    border-radius:0.3rem;
+    
+    
+    :hover{
+        background-color:var(--color-freela-hover);
+    }
+
+    >span{
+        font-size:22px;
+        font-weight:560;
+    }
+}
+`;
