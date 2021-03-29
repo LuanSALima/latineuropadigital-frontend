35335a9622ad0d9f8fa764dae051120db49605c2
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-03-29 13:04:11 -0300

    Toast Logic

---
+++ src/components/Toastifying/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Toast ,{ toast, ToastContainer } from "react-toastify";
+
+export const TOASTIFY_OPTIONS = {
+  position: "top-right",
+  autoClose: 8000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: false,
+  style: { fontSize: 16 },
+} ;
+
+function Toastifying(props){
+  let type = props.type ;
+   const TOASTIFY_OPTIONS = {
+    type,
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  }
+
+
+
+  toast(props.message, TOASTIFY_OPTIONS);
+
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  );
+};
+
+export default Toastifying;
