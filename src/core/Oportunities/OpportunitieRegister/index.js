2da97e8ee0d75111c05c96cebc3b570f0a826fb7
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-04-02 12:30:29 -0300

    Create modal component in on view

--- src/core/Oportunities/OpportunitieRegister/index.js
+++ src/core/Oportunities/OpportunitieRegister/index.js
@@ -8,65 +8,56 @@ import api from '../../../services/api';
 import { toast } from "react-toastify";
 
 import Select from 'react-select';
-import ModalTag from '../../../components/ModalTag';
+import {Modal,Button} from 'react-bootstrap'
+
+
 
 function OpportunitieRegister() {
 
-  const [buttonText, setButtonText] = useState("Cadastrar");
 
   const [professionalName, setProfessionalName] = useState("");
   const [professionalContact, setProfessionalContact] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [jobType, setJobType] = useState("");
   const [jobTypes, setJobTypes] = useState([]);
   const [dbJobTypes, setDbJobTypes] = useState([]);
   const [errors, setErrors] = useState({});
   const[modalShow,setModalShow] = useState(false);
 
-  useEffect(() => {
 
-    async function listTags() {
-      try {
-        const response = await api.get("/jobtype/list");
+  async function listTags() {
+    try {
+      const response = await api.get("/jobtype/list");
 
-        if(response.data.success) {
-          if(response.data.jobTypes) {
-            let dbJobTypes = [];
-            for(let index in response.data.jobTypes) {
-              dbJobTypes.push(response.data.jobTypes[index].title);
-            }
-            setDbJobTypes(dbJobTypes);
+      if(response.data.success) {
+        if(response.data.jobTypes) {
+          let dbJobTypes = [];
+          for(let index in response.data.jobTypes) {
+            dbJobTypes.push(response.data.jobTypes[index].title);
           }
-        }
-      } catch (error) {
-        if(error.response) {
-          if(error.response.data) {
-            if(error.response.data.message) {
-              setErrors({dbJobTypes: error.response.data.message});
-            }
-          }
+          setDbJobTypes(dbJobTypes);
         }
       }
+    } catch (error) {
+      toast.error("¡Hubo un error!",TOASTIFY_OPTIONS);
     }
-    listTags();
+  }
 
+  useEffect(() => {
+    listTags();
   }, []);
 
   const handleJobRegister = async (e) => {
     e.preventDefault();
-    setButtonText("Enviando Dados ...");
 
     try {
 
       
       const response = await api.post("/job/create", {professionalName, professionalContact, title, description, jobTypes});
       console.log(response.data);
-      setButtonText("Cadastrado com Sucesso");
       toast.success("¡Enviado para validación!",TOASTIFY_OPTIONS)
     } catch (error) {
       toast.error("¡Hubo un error! Inténtalo de nuevo.",TOASTIFY_OPTIONS)
-      setButtonText("Tente Novamente");
       if(error.response) {
         if(error.response.data) {
           //Dados retornados do backend
@@ -98,13 +89,47 @@ function OpportunitieRegister() {
     setModalShow(true);
   }
 
+
+
+
+  function MyModalView(props){
+   return ( <Modal
+    {...props}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+  >
+    <Toastifying/>
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">
+      <h1>Promociona tu trabajo</h1>
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+    {<h4>
+      Seleccione su rama de trabajo</h4>}
+                 
+
+      <Form width={"100%"} height={"50vh"} center nullBox nullBorder>
+      <ContentView>
+ {/* Promover RAMOS DE TABALHO AQUI! */}
+      <input type="text"/>
+</ContentView>
+  </Form>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button onClick={()=>setModalShow(false)}>Cerrar</Button>
+    </Modal.Footer>
+  </Modal>);
+  }
+
   return (
   <Page>
 
-{modalShow && <ModalTag
-    show={modalShow}
-    onHide={()=>setModalShow(false)}
-    />}
+        { <MyModalView 
+        show={modalShow}
+        onHide={()=>setModalShow(false)}
+        />}
 
     <Header/>
     <Form width={"45%"} height={"80vh"} center>
