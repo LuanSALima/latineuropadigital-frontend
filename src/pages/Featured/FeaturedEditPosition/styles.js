2ebc958f692094345620eae6dfc3393753741417
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-04-15 17:05:43 -0300

    Style / requisition in alter positions its ok

--- src/pages/Featured/FeaturedEditPosition/styles.js
+++ src/pages/Featured/FeaturedEditPosition/styles.js
@@ -2,8 +2,13 @@ import styled from 'styled-components';
 
 export const ListView = styled.div`
     display: flow-root;
-    width: 80%;
+    width: 90%;
+    margin-top:5px !important;
+    margin:0 auto;
     padding: 25px 40px;
+    justify-content:center;
+    justify-items:center;
+    background-color:var(--color-primary);
     text-align: center;
     margin: 0 auto;
     overflow-wrap: break-word;
