03fc903a4e41569bd48c65455fa93378ad84974d
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-04-19 16:17:00 -0300

    Finish  new  design for Oportunities

--- src/core/Oportunities/OpportunitieList/index.js
+++ src/core/Oportunities/OpportunitieList/index.js
@@ -1,46 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from '../../../components/Header';
-import { Page } from '../../../styles/default';
+import Header from "../../../components/Header";
+import { MyCardLink, Page } from "../../../styles/default";
 
-import api from '../../../services/api';
-import Footer from '../../../components/Footer';
-import { MyScreenView,OportunityCard } from './styles';
-
+import api from "../../../services/api";
+import Footer from "../../../components/Footer";
+import { MyScreenView } from "./styles";
+import { Link } from "react-router-dom";
+import NoticesCard from "../../../components/NoticesCard";
 
 function Job(props) {
+  const description =
+    "Professional Name: " +
+    props.job.professionalName +
+    "Descrição : " +
+    props.job.description +
+    "Contato : " +
+    props.job.professionalContact;
+
   return (
-    <OportunityCard>
-      <label>{props.job.title}</label>
-      <span >Nome: <b>{props.job.professionalName}</b></span>
-      <span >Descrição: <b>{props.job.description}</b></span>
-      <span >Contato: <b>{props.job.professionalContact}</b></span>
-    </OportunityCard>
+    <MyCardLink>
+      <Link to={" + content.id"}>
+        <NoticesCard date={""} title={props.job.title} text={description} />
+      </Link>
+    </MyCardLink>
   );
 }
 
 function OpportunitieList(props) {
-
   const [jobs, setJobs] = useState([]);
   const [errors, setErrors] = useState({});
-
-
 
   async function listJobs() {
     try {
       const response = await api.get("/job/list");
 
-      if(response.data.success) {
-        if(response.data.jobs) {
+      if (response.data.success) {
+        if (response.data.jobs) {
           setJobs(response.data.jobs);
         }
       }
     } catch (error) {
-      setErrors({message: "Não foi possível carregar as Oportunidades"});
-      if(error.response) {
-        if(error.response.data) {
-          if(error.response.data.message) {
-            setErrors({message: error.response.data.message});
+      setErrors({ message: "Não foi possível carregar as Oportunidades" });
+      if (error.response) {
+        if (error.response.data) {
+          if (error.response.data.message) {
+            setErrors({ message: error.response.data.message });
           }
         }
       }
@@ -52,19 +57,18 @@ function OpportunitieList(props) {
   }, []);
 
   return (
-  <Page>
-    <Header/>
-    <MyScreenView>
+    <Page>
+      <Header />
+      <MyScreenView>
+        <h1>Oportunidades</h1>
 
-    <h1>Oportunidades</h1>
-    
-     {jobs.map((currentjob)=>(
-        <Job job={currentjob} />
-      ))}
-
-    </MyScreenView>
-      <Footer/>
-  </Page>);
+        {jobs.map((currentjob) => (
+          <Job job={currentjob} />
+        ))}
+      </MyScreenView>
+      <Footer />
+    </Page>
+  );
 }
 
 export default OpportunitieList;
--- src/core/Oportunities/OpportunitieList/styles.js
+++ src/core/Oportunities/OpportunitieList/styles.js
@@ -20,39 +20,3 @@ export const MyScreenView = styled(ScreenView)`
 `;
 
 
-export const OportunityCard = styled.div`
-color:var(--color-freela-white);
-background-color:var(--color-freela-pink);
-font-size:18px;
-width:350px;
-min-height:250px;
-max-height:100vh;
-height:auto;
-margin-top:1rem;
-border-radius:0.5rem;
-padding:0 !important;
-cursor:pointer;
-transition:height 0.3s, width 0.3s ease;
-:hover{
-    width:355px;
-
-cursor:pointer;
-}
-
->label{
-    margin:0 auto !important;
-    font-size:26px;
-    font-weight:560;
-cursor:pointer;
-    padding:1.6rem;
-    text-transform:uppercase;
-}
->span{
-    padding:0.7rem 1rem ;
-    font-size:18px !important;
-    text-align:left;
-    margin:0 auto;
-    >b{
-    }
-}
-`;
