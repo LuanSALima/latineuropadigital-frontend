043e27bd3b34c5050b2cf36a3206e9be5fcc3b79
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-04-19 17:42:39 -0300

    Create fields and validate them

--- src/core/Event/EventRegister/index.js
+++ src/core/Event/EventRegister/index.js
@@ -37,6 +37,9 @@ function EventRegister() {
 
   const [firstRender,setFirstRender]= useState(true);
 
+  const [link,setLink] = useState();
+
+
   const handleValidator =  useMyForm(
     eventName,
     eventOrganizedBy,
@@ -310,6 +313,10 @@ function EventRegister() {
           </FormGroup>
         </FormColumn>
         <FormGroup>
+
+        <span>Por favor, inserte "http: //" o "https: //" antes de su enlace.</span>
+       <input style={!verifyLink(link) && !firstRender?{backgroundColor: '#f9b3b3'}:{}} type="text" placeholder="Link" onChange={(e)=>{setLink(e.target.value)}} value={link} />
+
           <label>Tags<Required>*</Required></label>
           <fieldset>
             <Select
